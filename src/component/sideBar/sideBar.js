0c102a3ac10d10c8a8b3eff52e62183d63d61c4d
sangram ghadage <sangramgha007@gmail.com>
2022-11-11 18:27:02 +0530

    first commit

---
+++ src/component/sideBar/sideBar.js
@@ -0,0 +1,325 @@
+import React, { useEffect, useState } from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { useNavigate } from 'react-router-dom';
+import { AppBar, Toolbar, CssBaseline,  Drawer, Divider, MenuItem, Menu, ListItem, ListItemIcon, ListItemText, List, IconButton, Avatar } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import HomeIcon from '@material-ui/icons/Home';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexGrow: 1,
+        backgroundColor: 'white',
+    },
+    title: {
+        flexGrow: 1,
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+        color: '#00A7EE'
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        fontFamily: '"Poppins", san-serif;',
+        fontStyle: 'normal',
+        fontWeight: 400,
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        backgroundColor: '#00A7EE',
+        fontFamily: '"Poppins", san-serif;',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        color: '#fff'
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(7) + 1,
+        },
+        backgroundColor: '#00A7EE',
+        color: '#fff'
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        fontFamily: '"Poppins", san-serif;',
+        fontStyle: 'normal',
+        fontWeight: 400,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(2),
+    },
+    appName: {
+        // flexGrow: 1,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(0),
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(0),
+        },
+        flex: 1,
+        textAlign: 'start',
+        color: '#00A7EE',
+        fontFamily: 'Poppins',
+        fontStyle: 'normal',
+        fontWeight: 800,
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+    },
+    paper: {
+        padding: theme.spacing(1),
+        color: '#78B088',
+        fontFamily: 'Poppins',
+        fontStyle: 'normal',
+        fontWeight: 800,
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+    },
+    report: {
+        justifyContent: 'center',
+        color: '#78B088',
+        fontFamily: '"San Francisco", Helvetica, Arial, san-serif;',
+        fontWeight: 'bold'
+    },
+    grid: {
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+    }
+}));
+
+
+export default function SideBar() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open1 = Boolean(anchorEl);
+    const [anchorElProfile, setAnchorElProfile] = React.useState(false);
+    const navigate = useNavigate();
+
+
+    const handleMenu = (event) => {
+        setAnchorElProfile(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorElProfile(true);
+        navigate("/#");
+    };
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    const menuItems = [
+        {
+            text: 'Dashboard',
+            icon: HomeIcon,
+            onClick: () => navigate("/#"),
+        },
+        {
+            text: 'Masters',
+            icon: DashboardIcon,
+            onClick: () => navigate("/#"),
+        },
+        {
+            text: "Vendors",
+            icon: PeopleOutlineIcon,
+            onClick: () => navigate('/#'),
+        },
+        {
+            text: "Reports",
+            icon: AssignmentIcon,
+            onClick: () => navigate("/#"),
+        },
+
+    ];
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+                style={{ backgroundColor: 'white' }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, {
+                            [classes.hide]: open,
+                        })}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={open1}
+                        onClose={handleClose}
+                    >
+                    </Menu>
+                    <div className={classes.appName}>
+                        <h1 style={{ fontSize: '1.5rem' }}>medcollapp</h1>
+                    </div>
+                    <div className={classes.drname}>
+
+                    </div>
+                    {auth && (
+                        <div className={classes.profile}>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="primary"
+
+                            >
+                                <Avatar style={{ borderRadius: 50, height: 40, width: 40 }} />
+                            </IconButton>
+
+
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElProfile}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={anchorElProfile}
+                                onClick={() => setAnchorElProfile(false)}
+                            >
+                                <MenuItem>Profile</MenuItem>
+                                <MenuItem>Logout</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
+                    <div style={{ marginRight: 10 }}>
+                        <center>
+                            <p style={{ color: '#fff', backgroundColor: 'red', position: 'absolute', top: 0, marginLeft: 14, marginTop: 5, border: '1px solid red', borderRadius: 50, width: '20px', fontSize: 12, fontWeight: 600, fontFamily: 'Poppins' }}>0 </p>
+                        </center>
+                        <NotificationsNoneIcon style={{ color: '#2C7FB2', cursor: 'pointer' }} />
+                    </div>
+
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                className={clsx(classes.drawer, {
+                    [classes.drawerOpen]: open,
+                    [classes.drawerClose]: !open,
+                })}
+                classes={{
+                    paper: clsx({
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    }),
+                }}
+            >
+                <div className={classes.toolbar} style={{ color: '#fff' }}>
+                    <IconButton onClick={handleDrawerClose} style={{ color: '#fff' }}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List onMouseOver={handleDrawerOpen} onMouseLeave={handleDrawerClose}>
+                    {menuItems.map(({ text, icon: Icon, onClick }, index) => (
+                        <ListItem button key={text} onClick={onClick}>
+                            <ListItemIcon style={{ color: '#fff' }}>
+                                <Icon />
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+                {/* <List onMouseOver={handleDrawerOpen} onMouseLeave={handleDrawerClose}>
+                    {menuItems.map(({ text, icon: Icon, onClick }, index) => (
+                        <ListItem button key={text} onClick={onClick}>
+                            <ListItemIcon style={{ color: '#fff' }}>
+                                <Icon />
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List> */}
+
+            </Drawer>
+        </div>
+    );
+}
